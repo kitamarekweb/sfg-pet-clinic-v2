d91b9e872549ccf10a1a7cb16128ee7c81e6955d
mk <kitamarekweb@gmail.com>
2021-02-04 12:01:00 +0100

    Initial Project Add

---
+++ src/main/java/guru/springframwork/sfgpetclinicv2/SfgPetClinicV2Application.java
@@ -0,0 +1,13 @@
+package guru.springframwork.sfgpetclinicv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframwork/sfgpetclinicv2/SfgPetClinicV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframwork.sfgpetclinicv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
